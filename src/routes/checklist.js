b3711c3f700c4ccf7b223712d4323870f5ff8ccb
Thiago95-br <thifb.95@gmail.com>
2023-02-27 20:55:49 -0300

    Adicionando as rotas no checklists.js, methods get post put e delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,4 +7,24 @@ router.get('/', (req, res) => {
     res.send();
 })
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(req.body)
+})
+
+router.get('/:id', (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+})
+
+router.put('/:id', (req, res) => {
+    console.log(req.body)
+    res.send(`PUT ID: ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.body)
+    res.send(`DELETE ID: ${req.params.id}`)
+})
+
 module.exports = router;
